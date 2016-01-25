1fd2178d68730c86cd9d8e5b864eddb06d79cf1f
Blair Peterson and Hillary Hintzen <blairchalmers@gmail.com>
2016-01-25 15:18:02 -0800

    complete scripts and jquery functionality for removing list items

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,19 @@ function Task(item,note) {
 }
 
 $ (document).ready(function() {
+  $("form#user-items").submit(function(){
+    event.preventDefault();
 
+    var userItem = $("input#new-item").val();
+    var userNote = $("input#new-note").val();
+    var newTask = new Task (userItem, userNote);
+
+    $("ul#to-do").append("<li><span class='list'>" + newTask.item + ": " + newTask.note + "</span></li>");
+
+    $("li").last().click(function() {
+      $(this).remove();
+    });
+
+
+  });
 });
